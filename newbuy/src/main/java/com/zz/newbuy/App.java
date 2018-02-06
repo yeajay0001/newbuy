770b455fd4782d31fb3e179d63e262ae4035d7da
xiuhua.zhang <82853589@qq.com>
2018-02-06 11:45:42 +0800

    init project init

---
+++ newbuy/src/main/java/com/zz/newbuy/App.java
@@ -0,0 +1,17 @@
+package com.zz.newbuy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+	private static final Logger logger= LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	logger.info("app start");
+    	SpringApplication.run(App.class, args);
+    }
+}
